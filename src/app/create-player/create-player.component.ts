4d27b92d21d00221b6fb058b42ae9988a773e5fa
Miguel Angel Pezonada <miguel.pezonada@gmail.com>
2023-09-29 23:03:23 +0200

    Fix color picking binding

--- src/app/create-player/create-player.component.ts
+++ src/app/create-player/create-player.component.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-player',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './create-player.component.html',
   styleUrls: ['./create-player.component.scss']
 })
 export class CreatePlayerComponent {
-
-  public myColor(): void {
-
-    (<any>document.getElementById('box')).value = (<HTMLInputElement>document.getElementById('colorPicker')).value;
-}
-
+  color = "#000000";
 }
